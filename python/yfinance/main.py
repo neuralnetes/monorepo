f29528410b6bc9a3d93638ed76554073d8ab5141
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-03-26 20:41:55 -0600

    requests

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -5,10 +5,13 @@ import os
 ticker = os.environ["YFINANCE_TICKER"]
 start = os.environ["YFINANCE_START"]
 end = os.environ["YFINANCE_END"]
+interval = os.environ["YFINANCE_INTERVAL"]
 csv_path = os.environ["YFINANCE_CSV_PATH"]
 
 
-def entry(a, b):
+
+def entry(request):
+    print(request)
     df = yf.download(ticker, start = start, end = end)
     df.to_csv(csv_path)
     ticker_df = pd.read_csv(csv_path)
