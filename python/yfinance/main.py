e17e373c36dc31c62c469d4865e728226396c98a
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-03-26 21:10:31 -0600

    removes junk

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -6,14 +6,8 @@ ticker = os.environ["YFINANCE_TICKER"]
 start = os.environ["YFINANCE_START"]
 end = os.environ["YFINANCE_END"]
 interval = os.environ["YFINANCE_INTERVAL"]
-csv_path = os.environ["YFINANCE_CSV_PATH"]
-
 
 
 def entry(request):
-    print(request)
-    df = yf.download(ticker, start = start, end = end)
-    df.to_csv(csv_path)
-    ticker_df = pd.read_csv(csv_path)
-    print(ticker_df.head())
-    print(ticker_df.tail())
+    df = yf.download(ticker, start = start, end = end, interval = interval)
+    return df.to_json()
