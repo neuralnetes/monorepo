345806577eb61b5bf0155595e1afffe24dd50f00
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-03-26 20:15:05 -0600

    Update main.py

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -7,6 +7,10 @@ start = os.environ["YFINANCE_START"]
 end = os.environ["YFINANCE_END"]
 csv_path = os.environ["YFINANCE_CSV_PATH"]
 
+
 def entry(a, b):
     df = yf.download(ticker, start = start, end = end)
     df.to_csv(csv_path)
+    ticker_df = pd.read_csv(csv_path)
+    print(ticker_df.head())
+    print(ticker_df.tail())
