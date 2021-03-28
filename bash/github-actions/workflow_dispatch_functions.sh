184470b5ee04c90b52dd5de9e5fb80cd50ece87f
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-03-27 21:53:59 -0400

    wip

--- bash/github-actions/workflow_dispatch_functions.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")"
-workflow=$(./find_workflow_by_path.sh ".github/workflows/gcs-source-archive-bucket-upload.yaml")
-workflow_id=$(echo "${workflow}" | jq '.id')
-./workflow_dispatch.sh "${workflow_id}" '{ "ref": "'"${GITHUB_REF}"'"}'
