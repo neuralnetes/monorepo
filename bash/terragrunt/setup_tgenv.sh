65f0666ae1cb16d4435cf4007a0757dca00602d2
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-03-28 04:40:09 -0400

    wip

--- bash/terragrunt/setup_tgenv.sh
+++ bash/terragrunt/setup_tgenv.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/cunymatthieu/tgenv.git "${GITHUB_USER_WORKSPACE}"/.tgenv
-ln -s "${GITHUB_USER_WORKSPACE}"/.tgenv/bin/* "${GITHUB_USER_WORKSPACE_BIN}"
+git clone https://github.com/cunymatthieu/tgenv.git "${GITHUB_WORKSPACE}"/.tgenv
+ln -s "${GITHUB_WORKSPACE}"/.tgenv/bin/* "${GITHUB_WORKSPACE_BIN}"
 tgenv install
