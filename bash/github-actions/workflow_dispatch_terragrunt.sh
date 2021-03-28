577b9022da4cae966b404cb9a1cbbaf95c74b993
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-03-27 21:29:45 -0400

    wip

--- bash/github-actions/workflow_dispatch_terragrunt.sh
+++ bash/github-actions/workflow_dispatch_terragrunt.sh
@@ -3,26 +3,16 @@ cd "$(dirname "$0")"
 request_json=$(
   jq -n \
     --arg ref "${GITHUB_REF}" \
-    --arg iam "${IAM}" \
-    --arg secret "${SECRET}" \
-    --arg network "${NETWORK}" \
-    --arg data "${DATA}" \
-    --arg compute "${COMPUTE}" \
-    --arg shared "${TERRAFORM}" \
     --arg terragrunt_working_dir "${TERRAGRUNT_WORKING_DIR}" \
     --arg terragrunt_run_all_command "${TERRAGRUNT_RUN_ALL_COMMAND}" \
+    --arg terragrunt_cli_flags "${TERRAGRUNT_CLI_FLAGS}" \
     '
       {
         "ref": $ref,
         "inputs": {
-          "network": $network,
-          "secret": $secret,
-          "iam": $iam,
-          "data": $data,
-          "compute": $compute,
-          "shared": $shared,
           "terragrunt_working_dir": $terragrunt_working_dir,
-          "terragrunt_run_all_command": $terragrunt_command
+          "terragrunt_run_all_command": $terragrunt_command,
+          "terragrunt_cli_flags": $terragrunt_cli_flags
         }
       }
     '
