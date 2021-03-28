e5f110a50f6ad652dc6a52d5d074fd05e812b0a2
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-03-28 08:16:03 -0400

    wip

--- terraform/modules/gcp/container-clusters/main.tf
+++ terraform/modules/gcp/container-clusters/main.tf
@@ -1,9 +1,6 @@
 module "container-clusters" {
   for_each                   = local.container_clusters_map
   source                     = "github.com/neuralnetes/monorepo.git//terraform/modules/gcp/container-cluster?ref=main"
-  add_cluster_firewall_rules = each.value["add_cluster_firewall_rules"]
-  create_service_account     = each.value["create_service_account"]
-  enable_private_nodes       = each.value["enable_private_nodes"]
   firewall_inbound_ports     = each.value["firewall_inbound_ports"]
   ip_range_pods              = each.value["ip_range_pods"]
   ip_range_services          = each.value["ip_range_services"]
@@ -18,7 +15,6 @@ module "container-clusters" {
   project_id                 = each.value["project_id"]
   region                     = each.value["region"]
   regional                   = each.value["regional"]
-  remove_default_node_pool   = each.value["remove_default_node_pool"]
   service_account            = each.value["service_account"]
   subnetwork                 = each.value["subnetwork"]
   zones                      = each.value["zones"]
