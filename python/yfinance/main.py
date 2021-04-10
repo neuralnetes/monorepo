295a2fdd4e7f615d1735c877b4f76f1bcfe83204
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-04-09 23:46:58 -0600

    Update main.py

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -1,3 +1,6 @@
+from google.cloud import storage
+from urllib import request
+
 import pandas as pd
 import yfinance as yf
 import os
@@ -6,8 +9,13 @@ ticker = os.environ["YFINANCE_TICKER"]
 start = os.environ["YFINANCE_START"]
 end = os.environ["YFINANCE_END"]
 interval = os.environ["YFINANCE_INTERVAL"]
-
+json_path = os.environ["YFINANCE_JSON_PATH"]
+bucket_name = os.environ["YFINANCE_BUCKET"]
+client = storage.Client()
+bucket = client.get_bucket(bucket_name)
 
 def entry(request):
     df = yf.download(ticker, start = start, end = end, interval = interval)
-    return df.to_json()
+   
+    bucket.blob(f'yfinance-dev/{ticker}.csv').upload_from_string(df.to_csv(), 'text/csv')
+    return "ok"
