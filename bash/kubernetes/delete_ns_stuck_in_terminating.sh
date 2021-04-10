8d7b9b1a48aca42c0b22722cf32cc01e084f1e93
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-10 18:16:26 -0400

    sleep 5

--- bash/kubernetes/delete_ns_stuck_in_terminating.sh
+++ bash/kubernetes/delete_ns_stuck_in_terminating.sh
@@ -5,6 +5,7 @@ NAMESPACE_JSON=$(kubectl get ns "${NAMESPACE}" -o json)
 NAMESPACE_EMPTY_FINALIZERS_JSON=$(echo "${NAMESPACE_JSON}" | jq ".spec.finalizers = []")
 kubectl proxy &
 PID=$!
+sleep 5
 curl -X PUT "http://localhost:8001/api/v1/namespaces/${NAMESPACE}/finalize" \
   -H "Content-Type: application/json" \
   -d "${NAMESPACE_EMPTY_FINALIZERS_JSON}"
