3b7dd8bdf6ac57d7b616565e54087cbddce2f404
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-11 00:04:20 -0400

    wip

--- terraform/modules/gcp/cloud-sqls/main.tf
+++ terraform/modules/gcp/cloud-sqls/main.tf
@@ -1,12 +1,15 @@
 module "mysqls" {
-  for_each         = local.mysqls_map
-  source           = "github.com/neuralnetes/monorepo.git//terraform/modules/gcp/cloud-sql-mysql?ref=main"
-  database_version = each.value["database_version"]
-  name             = each.value["name"]
-  project_id       = each.value["project_id"]
-  region           = each.value["region"]
-  zone             = each.value["zone"]
-  ip_configuration = each.value["ip_configuration"]
+  for_each                             = local.mysqls_map
+  source                               = "github.com/neuralnetes/monorepo.git//terraform/modules/gcp/cloud-sql-mysql?ref=main"
+  name                                 = each.value["name"]
+  tier                                 = each.value["tier"]
+  region                               = each.value["region"]
+  database_version                     = each.value["database_version"]
+  project                              = each.value["project"]
+  ip_configuration_private_network     = each.value["ip_configuration_private_network"]
+  ip_configuration_ipv4_enabled        = each.value["ip_configuration_ipv4_enabled"]
+  ip_configuration_authorized_networks = each.value["ip_configuration_authorized_networks"]
+  ip_configuration_require_ssl         = each.value["ip_configuration_require_ssl"]
 }
 
 module "postgresqls" {
