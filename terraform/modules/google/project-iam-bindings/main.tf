bbf379d7972f3c272dc64875dac6bd72fcc812c1
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-17 02:49:35 -0400

    uuit

--- terraform/modules/google/project-iam-bindings/main.tf
+++ terraform/modules/google/project-iam-bindings/main.tf
@@ -8,6 +8,6 @@ module "project-iam-bindings" {
 locals {
   project_iam_bindings_map = {
     for project_iam_binding in var.project_iam_bindings :
-    uuidv5() => project_iam_binding
+    uuid() => project_iam_binding
   }
 }
