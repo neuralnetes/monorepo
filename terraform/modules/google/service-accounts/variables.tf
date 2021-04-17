0d5d3f0711ad43ec3047a5debbae8c960a011e1d
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-17 01:24:33 -0400

    project

--- terraform/modules/google/service-accounts/variables.tf
+++ terraform/modules/google/service-accounts/variables.tf
@@ -1,13 +1,13 @@
 variable "service_accounts" {
   type = list(object({
-    project_id    = string
+    project    = string
     account_id = string
   }))
 }
 
 variable "service_account_datas" {
   type = list(object({
-    project_id    = string
+    project    = string
     account_id = string
   }))
 }
