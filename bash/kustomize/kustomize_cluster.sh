45531d267d8afbacca8151dbeec387aa8f7d1cd4
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-25 16:01:09 -0400

    cluster yamls

--- bash/kustomize/kustomize_cluster.sh
+++ bash/kustomize/kustomize_cluster.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 CLUSTER=$1
+PATHS=(
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/cert-manager/cert-manager/overlays/letsencrypt"
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/istio-install/base"
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/oidc-authservice/base"
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/dex/overlays/istio"
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/knative/knative-serving-install/base"
+  "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/kubeflow-istio-resources/base"
+  "kustomize/manifests/flux-kustomization/cluster/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/namespaces/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/cluster-role-binding/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/external-secrets/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/external-dns/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/secrets/kubeflow/overlays/${CLUSTER}"
+  "kustomize/manifests/flux-kustomization/kubeflow/1.3/overlays/${CLUSTER}"
+)
+
+for path in "${PATHS[@]}"; do
+  mkdir -p "${path}"
+done
 
 # cert-manager
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/cert-manager/cert-manager/overlays/letsencrypt/patch-cluster-issuer.yaml"
