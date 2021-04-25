8d10f5a4f422ddccb30c1d478fc210ec45ab9950
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-04-25 19:35:23 -0400

    wip

--- bash/kustomize/kustomize_cluster.sh
+++ bash/kustomize/kustomize_cluster.sh
@@ -22,6 +22,7 @@ done
 # cert-manager
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/cert-manager/cert-manager/overlays/letsencrypt/patch-cluster-issuer.yaml"
 kind: ClusterIssuer
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/cert-manager/cert-manager/overlays/letsencrypt/kustomization.yaml"
@@ -29,11 +30,13 @@ resources:
 - ../../../kubeflow/1.3/base/common/cert-manager/cert-manager/overlays/letsencrypt
 patchesStrategicMerge:
 - patch-cluster-issuer.yaml
+
 EOF
 
 # istio-install
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/istio-install/base/patch-service.yaml"
 kind: Service
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/istio-install/base/kustomization.yaml"
@@ -41,11 +44,13 @@ resources:
 - ../../../kubeflow/1.3/base/common/istio-1-9-0/istio-install/base
 patchesStrategicMerge:
 - patch-service.yaml
+
 EOF
 
 # oidc-authservice
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/oidc-authservice/base/patch-config.yaml"
 kind: ConfigMap
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/oidc-authservice/base/kustomization.yaml"
@@ -53,11 +58,13 @@ resources:
 - ../../../kubeflow/1.3/base/common/oidc-authservice/base
 patchesStrategicMerge:
 - patch-config.yaml
+
 EOF
 
 # dex
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/dex/overlays/istio/patch-config.yaml"
 kind: ConfigMap
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/dex/overlays/istio/kustomization.yaml"
@@ -65,11 +72,13 @@ resources:
 - ../../../kubeflow/1.3/base/common/dex/overlays/istio
 patchesStrategicMerge:
 - patch-config.yaml
+
 EOF
 
 # knative-serving-install
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/knative/knative-serving-install/base/patch-config.yaml"
 kind: ConfigMap
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/knative/knative-serving-install/base/kustomization.yaml"
@@ -77,11 +86,13 @@ resources:
 - ../../../kubeflow/1.3/common/knative/knative-serving-install/base
 patchesStrategicMerge:
 - patch-config.yaml
+
 EOF
 
 # kubeflow-istio-resources
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/kubeflow-istio-resources/base/patch-gateway.yaml"
 kind: Gateway
+
 EOF
 
 cat <<EOF > "kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/kubeflow-istio-resources/base/kustomization.yaml"
@@ -89,6 +100,7 @@ resources:
 - ../../../kubeflow/1.3/common/istio-1-9-0/kubeflow-istio-resources/base
 patchesStrategicMerge:
 - patch-gateway.yaml
+
 EOF
 
 # flux-kustomization
@@ -99,6 +111,7 @@ metadata:
   name: external-secrets
 spec:
   path: kustomize/manifests/external-secrets/overlays/${CLUSTER}
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/external-secrets/overlays/${CLUSTER}/kustomization.yaml"
@@ -106,6 +119,7 @@ resources:
 - ../../base
 patchesStrategicMerge:
 - patch-flux-kustomization.yaml
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/external-dns/overlays/${CLUSTER}/patch-flux-kustomization.yaml"
@@ -115,6 +129,7 @@ metadata:
   name: external-dns
 spec:
   path: kustomize/manifests/external-dns/overlays/${CLUSTER}
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/secrets/kubeflow/overlays/${CLUSTER}/kustomization.yaml"
@@ -122,6 +137,7 @@ resources:
 - ../../base
 patchesStrategicMerge:
 - patch-flux-kustomization.yaml
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/secrets/kubeflow/overlays/${CLUSTER}/patch-flux-kustomization.yaml"
@@ -131,6 +147,7 @@ metadata:
   name: kubeflow-secrets
 spec:
   path: kustomize/manifests/secrets/kubeflow/overlays/${CLUSTER}
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/secrets/kubeflow/overlays/${CLUSTER}/kustomization.yaml"
@@ -138,6 +155,7 @@ resources:
 - ../../base
 patchesStrategicMerge:
 - patch-flux-kustomization.yaml
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/kubeflow/1.3/overlays/${CLUSTER}/patch-flux-kustomization.yaml"
@@ -182,6 +200,7 @@ metadata:
   name: kubeflow-istio-resources
 spec:
   path: kustomize/manifests/kubeflow/1.3/overlays/${CLUSTER}/common/istio-1-9-0/kubeflow-istio-resources/base
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/kubeflow/1.3/overlays/${CLUSTER}/kustomization.yaml"
@@ -189,6 +208,7 @@ resources:
 - ../../base
 patchesStrategicMerge:
 - patch-flux-kustomization.yaml
+
 EOF
 
 cat <<EOF > "kustomize/manifests/flux-kustomization/cluster/overlays/${CLUSTER}/patch-flux-kustomization.yaml"
@@ -198,6 +218,7 @@ metadata:
   name: cluster
 spec:
   path: kustomize/manifests/flux-kustomization/cluster/overlays/${CLUSTER}
+
 EOF
 
 cat <<EOF > "kustomize/manifests/deploy/overlays/${CLUSTER}/kustomization.yaml"
@@ -205,4 +226,5 @@ namespace: flux-system
 resources:
 - ../../base
 - ../../../flux-kustomization/cluster/overlays/${CLUSTER}
+
 EOF
