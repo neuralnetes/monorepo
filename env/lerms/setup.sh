150ca4568d956f5880fe7591a11e4aecef81fa1b
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-15 15:58:52 -0400

    wip

--- env/lerms/setup.sh
+++ env/lerms/setup.sh
@@ -5,10 +5,14 @@ GITHUB_WORKSPACE="${GOPATH}/src/github.com/${GITHUB_REPOSITORY}"
 GITHUB_USER="lerms"
 GITHUB_USER_WORKSPACE="${GITHUB_WORKSPACE}/env/${GITHUB_USER}"
 SYMBOLIC_LINK_FILES=(
-  ".zshrc"
   ".envrc"
-  ".alias"
-  ".function"
+  ".envrc-alias"
+  ".envrc-base"
+  ".envrc-function"
+  ".envrc-github"
+  ".envrc-secret"
+  ".gitconfig"
+  ".zshrc"
 )
 for file in "${SYMBOLIC_LINK_FILES[@]}"; do
   ln -fs "${GITHUB_USER_WORKSPACE}/${file}" "${HOME}/${file}"
