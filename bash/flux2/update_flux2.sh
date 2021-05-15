8fe92a4029572bc3393a4b1203d62f20aad3a296
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-15 14:38:55 -0400

    wip

--- bash/flux2/update_flux2.sh
+++ bash/flux2/update_flux2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd "${GITHUB_WORKSPACE}/kustomize/flux-system/base"
+cd "${GITHUB_WORKSPACE}/kustomize/manifests/flux-system/base"
 wget "https://github.com/fluxcd/flux2/releases/download/${FLUX2_VERSION}/manifests.tar.gz"
 tar xvfz manifests.tar.gz
-git commit -am "update flux2 to version ${FLUX2_VERSION}"
+rm -rf manifests.tar.gz
+git add .
+git commit -m "update flux2 to version ${FLUX2_VERSION}"
