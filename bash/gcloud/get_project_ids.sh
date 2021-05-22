5ac1bf66988df8cede00598ba70bed8111dd69ad
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-22 01:55:35 -0400

    wip

--- bash/gcloud/get_project_ids.sh
+++ bash/gcloud/get_project_ids.sh
@@ -16,6 +16,7 @@ DATA_PROJECT=$(get_project_id_by_prefix "data" "${PROJECTS}")
 COMPUTE_PROJECT=$(get_project_id_by_prefix "compute" "${PROJECTS}")
 KUBEFLOW_PROJECT=$(get_project_id_by_prefix "kubeflow" "${PROJECTS}")
 CLUSTER_NAME=$(gcloud container clusters list --project="${COMPUTE_PROJECT}" --format=json | jq -rc '.[0].name')
+ISTIO_INGRESSGATEWAY_LOAD_BALANCER_IP="10.0.0.9"
 
 cat <<EOF > "${GITHUB_USER_WORKSPACE}/.envrc-projects"
 export IAM_PROJECT=${IAM_PROJECT}
@@ -25,6 +26,7 @@ export DATA_PROJECT=${DATA_PROJECT}
 export COMPUTE_PROJECT=${COMPUTE_PROJECT}
 export KUBEFLOW_PROJECT=${COMPUTE_PROJECT}
 export CLUSTER_NAME=${CLUSTER_NAME}
+export ISTIO_INGRESSGATEWAY_LOAD_BALANCER_IP=${ISTIO_INGRESSGATEWAY_LOAD_BALANCER_IP}
 EOF
 
 gcloud container clusters get-credentials --project="${COMPUTE_PROJECT}" --zone=us-central1-a "${CLUSTER_NAME}"
