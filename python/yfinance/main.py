a42f81c2570f59688fda1506b1817c7fc3af666c
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-05-23 14:42:34 -0600

    Update main.py
    Included ticker functionality in curl request

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -5,7 +5,6 @@ import pandas as pd
 import yfinance as yf
 import os
 
-ticker = os.environ["YFINANCE_TICKER"]
 start = os.environ["YFINANCE_START"]
 end = os.environ["YFINANCE_END"]
 interval = os.environ["YFINANCE_INTERVAL"]
@@ -16,7 +15,9 @@ bucket = client.get_bucket(bucket_name)
 
 def entry(request):
     request_json = request.get_json(silent=True)
+    ticker = request_json.get('ticker')
     df = yf.download(ticker, start = start, end = end, interval = interval)
     bucket.blob(f'yfinance-dev/{ticker}.csv').upload_from_string(df.to_csv(), 'text/csv')
     print(request_json)
+    print(ticker)
     return "ok"
