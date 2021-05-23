b8063fe93af01fc100e2611a0eaf0e12dcbcdcd6
Ian MacDonald <38594415+en-mac@users.noreply.github.com>
2021-05-23 15:15:48 -0600

    Update main.py (#4)
    Made code more pythonic and moved bucket into function. Removed artifacts

--- python/yfinance/main.py
+++ python/yfinance/main.py
@@ -1,23 +1,17 @@
 from google.cloud import storage
-from urllib import request
 
-import pandas as pd
 import yfinance as yf
-import os
 
-start = os.environ["YFINANCE_START"]
-end = os.environ["YFINANCE_END"]
-interval = os.environ["YFINANCE_INTERVAL"]
-json_path = os.environ["YFINANCE_JSON_PATH"]
-bucket_name = os.environ["YFINANCE_BUCKET"]
 client = storage.Client()
-bucket = client.get_bucket(bucket_name)
 
 def entry(request):
     request_json = request.get_json(silent=True)
     ticker = request_json.get('ticker')
+    start = request_json.get("start")
+    end = request_json.get("end")
+    interval = request_json.get("interval")
+    bucket_name = request_json.get("bucket")
+    bucket = client.get_bucket(bucket_name)
     df = yf.download(ticker, start = start, end = end, interval = interval)
     bucket.blob(f'yfinance-dev/{ticker}.csv').upload_from_string(df.to_csv(), 'text/csv')
-    print(request_json)
-    print(ticker)
     return "ok"
