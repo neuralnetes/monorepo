12671077e5bfee0cab51702b02f5c48ecf33617e
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-23 19:28:08 -0400

    wip

--- terraform/modules/google/compute-instances/variables.tf
+++ terraform/modules/google/compute-instances/variables.tf
@@ -1,9 +1,14 @@
 variable "compute_instances" {
   type = list(object({
-    name       = string
-    project    = string
-    location   = string
-    network    = string
-    subnetwork = string
+    name                  = string
+    machine_type          = string
+    image_project         = string
+    image_family          = string
+    project               = string
+    zone                  = string
+    network               = string
+    subnetwork            = string
+    service_account_email = string
+    tags                  = list(string)
   }))
 }
