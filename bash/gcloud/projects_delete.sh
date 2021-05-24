5a91e096bf997603d127bccfdc5314f3dc9ac52e
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-23 21:52:58 -0400

    wip

--- bash/gcloud/projects_delete.sh
+++ bash/gcloud/projects_delete.sh
@@ -1,44 +1,22 @@
 #!/bin/bash
-cd "$(dirname "$0")"
-PROJECTS=$(gcloud projects list --format=json | jq -rc  '.[]')
-
-projects=$(echo "${PROJECTS}" | jq -rc 'select(.projectId | contains("compute"))')
-project_ids=$(echo "${projects}" | jq -rc '.projectId')
-for project_id in ${project_ids}; do
+SERVICE_PROJECTS=(
+  "${IAM_PROJECT}"
+  "${SECRET_PROJECT}"
+  "${NETWORK_PROJECT}"
+  "${DATA_PROJECT}"
+  "${COMPUTE_PROJECT}"
+  "${KUBEFLOW_PROJECT}"
+)
+for project_id in "${SERVICE_PROJECTS[@]}"; do
   gcloud projects delete "${project_id}" -q
 done
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/compute/**/*"
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/us-central1/compute/**/*"
 
-projects=$(echo "${PROJECTS}" | jq -rc 'select(.projectId | contains("data"))')
-project_ids=$(echo "${projects}" | jq -rc '.projectId')
-for project_id in ${project_ids}; do
+HOST_PROJECTS=(
+  "${NETWORK_PROJECT}"
+)
+for project_id in "${HOST_PROJECTS[@]}"; do
+  . "${GITHUB_WORKSPACE}/bash/gcloud/gcloud_resource_manager_liens_delete.sh" "${project_id}"
   gcloud projects delete "${project_id}" -q
 done
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/data/**/*"
 
-projects=$(echo "${PROJECTS}" | jq -rc 'select(.projectId | contains("network"))')
-project_ids=$(echo "${projects}" | jq -rc '.projectId')
-for project_id in ${project_ids}; do
-  ./gcloud_resource_manager_liens_delete.sh "${project_id}"
-  gcloud projects delete "${project_id}" -q
-done
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/network/**/*"
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/us-central1/network/**/*"
-
-projects=$(echo "${PROJECTS}" | jq -rc 'select(.projectId | contains("secret"))')
-project_ids=$(echo "${projects}" | jq -rc '.projectId')
-for project_id in ${project_ids}; do
-  gcloud projects delete "${project_id}" -q
-done
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/secret/**/*"
-
-projects=$(echo "${PROJECTS}" | jq -rc 'select(.projectId | contains("iam"))')
-project_ids=$(echo "${projects}" | jq -rc '.projectId')
-for project_id in ${project_ids}; do
-  gcloud projects delete "${project_id}" -q
-done
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/iam/**/*"
-
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/global/terraform/**/*"
-gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/**/*"
+gsutil -m -q  rm -rf  "gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/non-prod/**/*"
