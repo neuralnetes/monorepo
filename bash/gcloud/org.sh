572c0a33631129910dc5ad9085fc1ed3ed89dfb9
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-28 19:01:32 -0400

    wip

--- bash/gcloud/org.sh
+++ bash/gcloud/org.sh
@@ -4,47 +4,95 @@ ORGANIZATION_IAM_ROLES=(
   "roles/billing.admin"
   "roles/compute.admin"
   "roles/container.admin"
+  "roles/iam.organizationRoleAdmin"
   "roles/resourcemanager.folderAdmin"
   "roles/resourcemanager.organizationAdmin"
   "roles/resourcemanager.projectCreator"
   "roles/serviceusage.serviceUsageAdmin"
   "roles/storage.admin"
 )
-ROOT_PROJECT="terraform-neuralnetes"
-SERVICE_ACCOUNT_KEY="key.json"
+PROJECT="terraform-neuralnetes"
 SERVICE_ACCOUNT_NAME="terraform"
-SERVICE_ACCOUNT_EMAIL="terraform@${ROOT_PROJECT}.iam.gserviceaccount.com"
-GROUP_EMAIL="${SERVICE_ACCOUNT_NAME}@${ORGANIZATION}"
+SERVICE_ACCOUNT_EMAIL="${SERVICE_ACCOUNT_NAME}@${PROJECT}.iam.gserviceaccount.com"
 
-# TODO: create terraform google group
-gcloud projects create "${ROOT_PROJECT}"
+echo "log in with super administrator google account"
+gcloud auth login
+
+ORGANIZATION_ID=$(
+  gcloud organizations list --format=json |
+    jq -rc --arg display_name "${ORGANIZATION}" \
+      '.[] | select(.displayName == $display_name) | .name | split("/") | last'
+)
+
+gcloud projects create "${PROJECT}"
+
+PROJECT_SERVICES=(
+  "bigquery.googleapis.com"
+  "bigquerystorage.googleapis.com"
+  "cloudbilling.googleapis.com"
+  "cloudresourcemanager.googleapis.com"
+  "cloudtrace.googleapis.com"
+  "compute.googleapis.com"
+  "container.googleapis.com"
+  "containerregistry.googleapis.com"
+  "deploymentmanager.googleapis.com"
+  "iam.googleapis.com"
+  "iamcredentials.googleapis.com"
+  "logging.googleapis.com"
+  "monitoring.googleapis.com"
+  "oslogin.googleapis.com"
+  "pubsub.googleapis.com"
+  "servicenetworking.googleapis.com"
+  "serviceusage.googleapis.com"
+  "sqladmin.googleapis.com"
+  "storage-api.googleapis.com"
+  "storage.googleapis.com"
+)
+
+for project_service in "${PROJECT_SERVICES[@]}"; do
+  echo "enabling ${project_service}"
+  gcloud services enable "${project_service}" \
+    --project="${PROJECT}"
+done
+
 gcloud iam service-accounts create "${SERVICE_ACCOUNT_NAME}" \
   --description="${SERVICE_ACCOUNT_NAME}" \
   --display-name="${SERVICE_ACCOUNT_NAME}" \
-  --project="${ROOT_PROJECT}"
-
-gcloud iam service-accounts keys create "${SERVICE_ACCOUNT_KEY}" \
-  --iam-account="${SERVICE_ACCOUNT_EMAIL}" \
-  --project="${ROOT_PROJECT}"
-
-# TODO: cat key.json | pbcopy upload to github actions secret
+  --project="${PROJECT}"
 
 for role in "${ORGANIZATION_IAM_ROLES[@]}"; do
-  gcloud organizations add-iam-policy-binding "${ORGANIZATION}" \
-    --member="group:${GROUP_EMAIL}" \
+  gcloud organizations add-iam-policy-binding "${ORGANIZATION_ID}" \
+    --member="serviceAccount:${SERVICE_ACCOUNT_EMAIL}" \
     --role="${role}"
 done
 
-# USER_GROUP_EMAIL="${SERVICE_ACCOUNT_NAME}-users@${ORGANIZATION}"
-#SERVICE_ACCOUNT_USERS=(
-#  "alexander.lerma@${ORGANIZATION}"
-#)
-#gcloud iam service-accounts add-iam-policy-binding \
-#  "${SERVICE_ACCOUNT_EMAIL}" \
-#  --member="group:${USER_GROUP_EMAIL}" \
-#  --role="roles/iam.serviceAccountUser"
-#
-#for service_account_user in "${SERVICE_ACCOUNT_USERS[@]}"; do
-#  # TODO: add users to terraform-users@neuralnetes.com group
-#  echo "add ${service_account_user} to ${USER_GROUP_EMAIL}"
-#done
+SERVICE_ACCOUNT_KEY="key.json"
+gcloud iam service-accounts keys create "${SERVICE_ACCOUNT_KEY}" \
+  --iam-account="${SERVICE_ACCOUNT_EMAIL}" \
+  --project="${PROJECT}"
+
+echo "store this secret in github secret"
+cat "${SERVICE_ACCOUNT_KEY}"
+
+TERRAFORM_GROUP_EMAIL="${SERVICE_ACCOUNT_NAME}@${ORGANIZATION}"
+TERRAFORM_GROUP_ROLES=(
+  "roles/iam.serviceAccountUser"
+  "roles/iam.serviceAccountTokenCreator"
+)
+#ACCESS_TOKEN=$(gcloud auth print-access-token)
+# TODO: automate creation of terraform google group via groups REST api
+# https://www.googleapis.com/admin/directory/v1/groups
+# https://cloud.google.com/identity/docs/reference/rest/v1/groups/create
+# https://cloud.google.com/identity/docs/reference/rest/v1/groups.memberships/create
+#echo "create google group ${TERRAFORM_GROUP_EMAIL}"
+#curl --request POST \
+#     --header "Content-Type: application/json" \
+#     --header "Authorization: Bearer ${ACCESS_TOKEN}" \
+#     --data '{"email":"${SERVICE_ACCOUNT_NAME}@${ORGANIZATION}","name":"terraform","description": "terraform"}'
+
+for role in "${TERRAFORM_GROUP_ROLES[@]}"; do
+  gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT_EMAIL}" \
+    --member="group:${TERRAFORM_GROUP_EMAIL}" \
+    --role="${role}" \
+    --project="${PROJECT}"
+done
