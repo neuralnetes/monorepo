71873b102799373d075c93afc28e825e6a92f577
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-28 17:49:22 -0400

    wip

--- bash/gcloud/org.sh
+++ bash/gcloud/org.sh
@@ -11,6 +11,7 @@ ORGANIZATION_IAM_ROLES=(
   "roles/storage.admin"
 )
 ROOT_PROJECT="terraform-neuralnetes"
+SERVICE_ACCOUNT_KEY="key.json"
 SERVICE_ACCOUNT_NAME="terraform"
 SERVICE_ACCOUNT_EMAIL="terraform@${ROOT_PROJECT}.iam.gserviceaccount.com"
 SERVICE_ACCOUNT_USERS=(
@@ -25,10 +26,11 @@ gcloud iam service-accounts create "${SERVICE_ACCOUNT_NAME}" \
   --display-name="${SERVICE_ACCOUNT_NAME}" \
   --project="${ROOT_PROJECT}"
 
-gcloud iam service-accounts add-iam-policy-binding \
-  "${SERVICE_ACCOUNT_EMAIL}" \
-  --member="group:${USER_GROUP_EMAIL}" \
-  --role="roles/iam.serviceAccountUser"
+gcloud iam service-accounts keys create "${SERVICE_ACCOUNT_KEY}" \
+  --iam-account="${SERVICE_ACCOUNT_EMAIL}" \
+  --project="${ROOT_PROJECT}"
+
+# TODO: cat key.json | pbcopy upload to github actions secret
 
 for role in "${ORGANIZATION_IAM_ROLES[@]}"; do
   gcloud organizations add-iam-policy-binding "${ORGANIZATION}" \
@@ -36,7 +38,12 @@ for role in "${ORGANIZATION_IAM_ROLES[@]}"; do
     --role="${role}"
 done
 
-for service_account_user in "${SERVICE_ACCOUNT_USERS[@]}"; do
-  # TODO: add users to terraform-users@neuralnetes.com group
-  echo "add ${service_account_user} to ${USER_GROUP_EMAIL}"
-done
+#gcloud iam service-accounts add-iam-policy-binding \
+#  "${SERVICE_ACCOUNT_EMAIL}" \
+#  --member="group:${USER_GROUP_EMAIL}" \
+#  --role="roles/iam.serviceAccountUser"
+#
+#for service_account_user in "${SERVICE_ACCOUNT_USERS[@]}"; do
+#  # TODO: add users to terraform-users@neuralnetes.com group
+#  echo "add ${service_account_user} to ${USER_GROUP_EMAIL}"
+#done
