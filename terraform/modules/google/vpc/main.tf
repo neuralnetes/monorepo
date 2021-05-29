04d5fb72d2b048bbe03e86b8e572fd0f0cc2ccb7
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-05-29 15:38:43 -0400

    fix

--- terraform/modules/google/vpc/main.tf
+++ terraform/modules/google/vpc/main.tf
@@ -1,10 +1,11 @@
 module "vpc" {
   for_each                               = local.vpc_map
   source                                 = "github.com/terraform-google-modules/terraform-google-network.git//modules/vpc?ref=v3.2.2"
-  network_name                           = each.value["network_name"]
-  project_id                             = each.value["project_id"]
   auto_create_subnetworks                = each.value["auto_create_subnetworks"]
   delete_default_internet_gateway_routes = each.value["delete_default_internet_gateway_routes"]
+  network_name                           = each.value["network_name"]
+  project_id                             = each.value["project_id"]
+  routing_mode                           = each.value["routing_mode"]
   shared_vpc_host                        = each.value["shared_vpc_host"]
 }
 
--- terraform/modules/google/vpc/variables.tf
+++ terraform/modules/google/vpc/variables.tf
@@ -1,9 +1,10 @@
 variable "vpc" {
   type = list(object({
-    network_name                           = string
-    project_id                             = string
     auto_create_subnetworks                = string
     delete_default_internet_gateway_routes = string
+    network_name                           = string
+    project_id                             = string
+    routing_mode                           = string
     shared_vpc_host                        = string
   }))
 }
