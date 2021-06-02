a787038b35fe397902f804bf29617387fa6a2c56
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-06-01 22:39:16 -0400

    wip

--- bash/kubernetes/flux_reconcile.sh
+++ bash/kubernetes/flux_reconcile.sh
@@ -4,5 +4,6 @@ kubectl get kustomizations.kustomize.toolkit.fluxcd.io -n flux-system -o json \
     --arg github_sha "${GITHUB_SHA}" \
     '.items[] | select(.status.conditions | first | .message | split("/") | last != $github_sha) | .metadata.name' \
   | while read -r kustomization_name; do
-      flux reconcile kustomization "${kustomization_name}"
+      flux reconcile kustomization "${kustomization_name}" &
     done
+wait
