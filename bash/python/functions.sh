3c70a6607f35ee0c031aa30564592ee83257f07a
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-06-13 07:05:57 -0400

    wip

--- bash/python/functions.sh
+++ bash/python/functions.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 function setup_pyenv() {
   export PYENV_ROOT="$HOME/.pyenv"
-  rm -rf "${PYENV_ROOT}"
-  git clone https://github.com/pyenv/pyenv.git "${PYENV_ROOT}"
+  if [[ ! -d "${PYENV_ROOT}" ]]; then
+      git clone https://github.com/pyenv/pyenv.git "${PYENV_ROOT}"
+  fi
   eval "$(pyenv init --path)"
   eval "$(pyenv init -)"
 }
 
 function setup_pyenv_virtualenv() {
-  PYTHON_VERSION=$(cat "${GITHUB_WORKSPACE}/.python-version")
-  pyenv install "${PYTHON_VERSION}"
-  git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+  DIR=$(pyenv root)/plugins/pyenv-virtualenv
+  if [[ ! -d "${DIR}" ]]; then
+      git clone https://github.com/pyenv/pyenv.git "${DIR}"
+  fi
   eval "$(pyenv virtualenv-init -)"
-  source "${HOME}/.zshrc"
-  pyenv virtualenv -f "${PYTHON_VERSION}" "${PYTHON_VERSION}-monorepo"
-  pyenv activate "${PYTHON_VERSION}-monorepo"
 }
