efcf6cfbd283ac884c9bf7c292ea34564c381a34
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-06-14 01:44:30 -0400

    wip

--- bash/github/functions.sh
+++ bash/github/functions.sh
@@ -252,21 +252,15 @@ function post_github_workflow_dispatch_terragrunt_non_prod() {
 
 function post_slack_event() {
     STATUS=$1
-    export GCS_TERRAFORM_REMOTE_STATE_BUCKET_LOGS="gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/${GITHUB_SHA}/logs.txt"
-    export GCS_TERRAFORM_REMOTE_STATE_BUCKET_ENV="gs://${GCS_TERRAFORM_REMOTE_STATE_BUCKET}/${GITHUB_SHA}/env.txt"
     TEXT=$(
       jq -n \
-        --arg logs "${GCS_TERRAFORM_REMOTE_STATE_BUCKET_LOGS}" \
-        --arg env "${GCS_TERRAFORM_REMOTE_STATE_BUCKET_ENV}" \
         --arg run "${GITHUB_ACTIONS_RUN_URL}" \
         --arg status "${STATUS}" \
         '{
           "type": "github_actions",
           "data": {
             "status": $status,
-            "logs": $logs,
-            "run": $run,
-            "env": $env
+            "run": $run
           }
         }'
     )
@@ -277,9 +271,6 @@ function post_slack_event() {
           "text": ($text | tostring)
         }'
     )
-    env >"${GITHUB_WORKSPACE}/env.txt"
-    gsutil cp "${GITHUB_WORKSPACE}/logs.txt" "${GCS_TERRAFORM_REMOTE_STATE_BUCKET_LOGS}"
-    gsutil cp "${GITHUB_WORKSPACE}/env.txt" "${GCS_TERRAFORM_REMOTE_STATE_BUCKET_ENV}"
     post_slack_webhook
 }
 
