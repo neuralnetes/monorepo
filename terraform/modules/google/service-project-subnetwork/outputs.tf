510d1b941233ccc13a61cc458f9feae5082e509d
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-07-05 18:37:20 -0500

    outputs

--- terraform/modules/google/service-project-subnetwork/outputs.tf
+++ terraform/modules/google/service-project-subnetwork/outputs.tf
@@ -5,3 +5,7 @@ output "api_service_account_email" {
 output "project_service_account_email" {
   value = local.project_service_account_email
 }
+
+output "shared_vpc_access" {
+  value = module.shared_vpc_access
+}
