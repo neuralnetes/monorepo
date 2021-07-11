5c0b666056664fa5db4f31040f3aa03874757f9f
Alexander Lerma <alexander.lerma@neuralnetes.com>
2021-07-11 13:37:08 -0500

    crds

--- bash/kubernetes/generate_istio.sh
+++ bash/kubernetes/generate_istio.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 export ISTIO_VERSION=1.10.2
+export DOWNLOAD_DIR="istio-${ISTIO_VERSION}"
+export OUTPUT_DIR=kustomize/manifests
 curl -L https://istio.io/downloadIstio | sh -
-helm template istio-operator "istio-${ISTIO_VERSION}/manifests/charts/istio-operator" \
-  --set operatorNamespace=istio-operator --output-dir kustomize/manifests
-rm -rf istio-${ISTIO_VERSION}
-mkdir -p kustomize/manifests/istio-operator/base
-mv kustomize/manifests/istio-operator/templates/* kustomize/manifests/istio-operator/base
-rm -rf kustomize/manifests/istio-operator/templates
+helm template istio-operator "${DOWNLOAD_DIR}/manifests/charts/istio-operator" \
+  --set operatorNamespace=istio-operator \
+  --output-dir "${OUTPUT_DIR}"
+mkdir -p "${OUTPUT_DIR}/istio-operator/base"
+mv "${DOWNLOAD_DIR}/manifests/charts/istio-operator/crds/crd-operator.yaml" "${OUTPUT_DIR}/istio-operator/base"
+mv "${OUTPUT_DIR}/istio-operator/templates"/* "${OUTPUT_DIR}/istio-operator/base"
+rm -rf "${OUTPUT_DIR}/istio-operator/templates"
+
 # istio-operator
-cat <<EOF >"kustomize/manifests/istio-operator/base/kustomization.yaml"
+cat <<EOF >"${OUTPUT_DIR}/istio-operator/base/kustomization.yaml"
 resources:
+- crd-operator.yaml
 - clusterrole.yaml
 - clusterrole_binding.yaml
 - deployment.yaml
@@ -17,9 +22,10 @@ resources:
 - service.yaml
 - service_account.yaml
 EOF
+
 # istio-system
-mkdir -p kustomize/manifests/istio-system/base
-cat <<EOF >"kustomize/manifests/istio-system/base/istio-operator.yaml"
+mkdir -p "${OUTPUT_DIR}/istio-system/base"
+cat <<EOF >"${OUTPUT_DIR}/istio-system/base/istio-operator.yaml"
 apiVersion: install.istio.io/v1alpha1
 kind: IstioOperator
 metadata:
@@ -27,8 +33,11 @@ metadata:
 spec:
   profile: default
 EOF
-cat <<EOF >"kustomize/manifests/istio-system/base/kustomization.yaml"
+
+cat <<EOF >"${OUTPUT_DIR}/istio-system/base/kustomization.yaml"
 namespace: istio-system
 resources:
 - istio-operator.yaml
 EOF
+
+rm -rf "${DOWNLOAD_DIR}"
